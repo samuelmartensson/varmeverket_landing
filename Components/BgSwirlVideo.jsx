05e3b50e331d43826e128e8efdbee35f35b6433e
Samuel Mårtensson <samuel137@live.se>
2022-11-12 18:00:12 +0100

    bg fix

--- Components/BgSwirlVideo.jsx
+++ Components/BgSwirlVideo.jsx
@@ -1,10 +1,17 @@
-import React, { Component } from "react";
-export default class BgSwirlVideo extends Component {
-  render() {
-    return (
-      <video className="video-bg" autoPlay muted loop id="video">
-        <source src={this.props.source} type="video/mp4" />
-      </video>
-    );
-  }
-}
+import React, { useEffect, useRef } from "react";
+
+const BgSwirlVideo = ({ source }) => {
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current?.play();
+  }, []);
+
+  return (
+    <video ref={ref} className="video-bg" autoPlay muted loop id="video">
+      <source src={source} type="video/mp4" />
+    </video>
+  );
+};
+
+export default BgSwirlVideo;
